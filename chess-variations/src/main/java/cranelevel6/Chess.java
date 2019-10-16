023556772954904f189db0c166aa5d64ff513c77
league <league@imac-12.attlocal.net>
2019-10-15 18:22:04 -0700

    board

--- chess-variations/src/main/java/cranelevel6/Chess.java
+++ chess-variations/src/main/java/cranelevel6/Chess.java
@@ -8,6 +8,7 @@ public class Chess {
 		//create and place pieces
 		//start game
 		Board board = new Board();
+		System.out.println(board.getTiles()[0][0].getxCord());
 	}
 	
 	public static void main(String[] args) {
