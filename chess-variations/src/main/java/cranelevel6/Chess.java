f0d78756e11cd94514044e1840a26f3a34309cd0
league <league@imac-9.attlocal.net>
2019-10-22 18:00:36 -0700

    updatate

--- chess-variations/src/main/java/cranelevel6/Chess.java
+++ chess-variations/src/main/java/cranelevel6/Chess.java
@@ -54,7 +54,6 @@ public class Chess implements ActionListener {
 		//create and place pieces
 		//start game
 		Board board = new Board();
-		System.out.println(board.getTiles()[0][0].getxCord());
 	}
 	
 	public static void main(String[] args) {
