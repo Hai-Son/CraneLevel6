b55d39b8ad1af80cd2c2d51cea947f2661f5034d
league <league@imac-24.attlocal.net>
2019-10-22 18:25:26 -0700

    minor fix to chess class

--- chess-variations/src/main/java/cranelevel6/Chess.java
+++ chess-variations/src/main/java/cranelevel6/Chess.java
@@ -10,8 +10,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Chess implements ActionListener {
-	int introWidth = 800;
-	int introHeight = 800;
+	int introWidth = 900;
+	int introHeight = 900;
 	JButton multiplayer;
 	JButton singleplayer;
 	JButton startGame;
@@ -23,9 +23,9 @@ public class Chess implements ActionListener {
 		frame.setSize(introWidth, introHeight);
 		
 		//buttons
-		JButton multiplayer = new JButton();
-		JButton singleplayer = new JButton();
-		JButton startGame = new JButton();
+		 multiplayer = new JButton();
+		 singleplayer = new JButton();
+		 startGame = new JButton();
 		
 		multiplayer.addActionListener(this);
 		singleplayer.addActionListener(this);
@@ -34,22 +34,19 @@ public class Chess implements ActionListener {
 		multiplayer.setText("Multiplayer");
 		singleplayer.setText("Singleplayer");
 		startGame.setText("Start Game");
-
+		//
 		
 		frame.add(panel);
 		panel.add(singleplayer);
 		panel.add(multiplayer);
 		panel.add(startGame);
-		
 
-		
 		frame.setVisible(true);
 		
 	}
 	
 	public void setup(){
 		System.out.println("running setup");
-		//Opening screen?
 		//create and place squares
 		//create and place pieces
 		//start game
@@ -66,7 +63,6 @@ public class Chess implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == startGame){
-			//this isn't running the setup yet, not sure why
 			setup();
 		}
 	}
