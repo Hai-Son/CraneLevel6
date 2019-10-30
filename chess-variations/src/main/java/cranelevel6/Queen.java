bfa3dba2c5a45c393476da863e813886c9e01101
league <league@imac-12.attlocal.net>
2019-10-29 18:28:34 -0700

    Queen

--- chess-variations/src/main/java/cranelevel6/Queen.java
+++ chess-variations/src/main/java/cranelevel6/Queen.java
@@ -3,11 +3,12 @@ package cranelevel6;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Queen extends Piece {
 	int x = 5;
 	int y = 2;
 	String coordinates = "";
-	List<Directions> legalmoves = new ArrayList<>();
+	List<Directions> legalmoves = new ArrayList<Directions>();
 
 	public static void main(String[] args) {
 		Queen Test = new Queen();
@@ -24,22 +25,25 @@ public class Queen extends Piece {
 	}
 
 	public List<Location> getPotential() {
-		// return coordinates;
-		int index = 0;
-		List<Location> moves = new ArrayList<>();
-		Location current = getLocation(); // Gets position of piece.
-		for (Directions d : legalmoves) {
-			Location n = current.getLocation(d); // Location of object
-			while (index < 1) {
-				Location s = new Location(n.getxTile() + 1, n.getyTile());
-				if (s.getHasPiece() == false) {
-					moves.add(s);
-				} else {
-					index += 1; //Break 
-				}
-			}
+//		// return coordinates;
+//		int index = 0;
+//		//List<Location> moves = new ArrayList<>();
+//		Location current = getLocation(); // Gets position of piece.
+//		for (Directions d : legalmoves) {
+//			Location n = current.getLocation(d); // Location of object
+//			while (index < 1) {
+//				Location s = new Location(n.getxTile() + 1, n.getyTile());
+//				if (s.getHasPiece() == false) {
+//					moves.add(s);
+//				} else {
+//					index += 1; //Break 
+//				}
+//			}
+//		}
+		if(Directions.N) {
+			
 		}
-		return moves;
+
 	}
 	// recursively add another item as we go
 	// until null or smth is hit
