f080f2e7bbe9f0eecb9b764c13c677714d959b39
League Student <league@iMac.attlocal.net>
2019-11-05 17:45:04 -0800

    Update Castle.java
    Castle(IP) 5:44

--- chess-variations/src/main/java/cranelevel6/Castle.java
+++ chess-variations/src/main/java/cranelevel6/Castle.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Castle extends Piece{
 	Location location;
-	int x; // (0,0) or (8,0)
+	int x; // (0,0) or (7,0)for white?
 	int y = 0;
 	
 	ArrayList<Directions> moves = new ArrayList<Directions>();
@@ -19,10 +19,35 @@ public class Castle extends Piece{
 	
 	Castle c = new Castle();
 	
-	public void direction() {
+	public void restrictions() {
 		
-		/////START HEREEE
+		if(c.getLocation().getxTile() == 7) {
+			moves.remove(Directions.E);
+		}
+		if(c.getLocation().getxTile() == 0) {
+			moves.remove(Directions.W);
+		}
+		if(c.getLocation().getyTile() == 7) {
+			moves.remove(Directions.S);
+		}
+		if(c.getLocation().getyTile() == 0) {
+			moves.remove(Directions.N);
+		}
 		
+	}
+
+	public ArrayList<Location> getPossibleMoves(){
+		ArrayList<Location> possibleMoves = new ArrayList<Location>();
+		for(Directions d: moves) {
+			Location l = location.getLocation(d);
+			while(l != null) {
+				possibleMoves.add(l);
+				l = l.getLocation(d);
+			}
+		}
+		
+		
+		return possibleMoves;
 	}
 	
 	public static void main(String[] args) {
