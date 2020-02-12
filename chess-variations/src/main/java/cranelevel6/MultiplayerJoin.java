80293fa26855c6dcbdf954ffe4e8b5b1bcc4e3f1
League Student <league@iMac-24.attlocal.net>
2020-02-11 18:14:51 -0800

    multiplayer 2

--- chess-variations/src/main/java/cranelevel6/MultiplayerJoin.java
+++ chess-variations/src/main/java/cranelevel6/MultiplayerJoin.java
@@ -9,8 +9,11 @@ import javax.swing.JOptionPane;
 
 public class MultiplayerJoin {
 	String hostIP;
+	Board board;
+	Chess c;
 
-	public MultiplayerJoin() throws IOException {
+	public MultiplayerJoin(Chess c) throws IOException {
+		this.c = c;
 		hostIP = JOptionPane.showInputDialog(null, "Enter Your Opponent's IP Adress", "Multiplayer Connect",
 				JOptionPane.PLAIN_MESSAGE);
 	}
@@ -18,12 +21,15 @@ public class MultiplayerJoin {
 	public void run() {
 		Integer port = 8585;
 		try {
-			Socket sock = new Socket(hostIP, port);
-			DataOutputStream streamOut = new DataOutputStream(sock.getOutputStream());
+			Socket socket = new Socket(hostIP, port);
+			Board board = new Board(c);
+			JOptionPane.showMessageDialog(null, "You connected to the game", "Multiplayer Connect",
+					JOptionPane.PLAIN_MESSAGE, null);
+			DataOutputStream streamOut = new DataOutputStream(socket.getOutputStream());
 			streamOut.writeUTF("join test");
-			DataInputStream streamIn = new DataInputStream(sock.getInputStream());
+			DataInputStream streamIn = new DataInputStream(socket.getInputStream());
 			System.out.println(streamIn.readUTF());
-			sock.close();
+			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
