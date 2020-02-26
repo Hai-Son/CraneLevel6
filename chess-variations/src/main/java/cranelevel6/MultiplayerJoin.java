92d8f3827a606f4b48baed323d013bcf8cc48de0
League Student <league@iMac-24.attlocal.net>
2020-02-25 18:26:55 -0800

    multiplayer 5

--- chess-variations/src/main/java/cranelevel6/MultiplayerJoin.java
+++ chess-variations/src/main/java/cranelevel6/MultiplayerJoin.java
@@ -34,14 +34,31 @@ public class MultiplayerJoin {
 					JOptionPane.PLAIN_MESSAGE, null);
 			DataOutputStream streamOut = new DataOutputStream(socket.getOutputStream());
 			DataInputStream streamIn = new DataInputStream(socket.getInputStream());
+			Piece p = null;
 			while (live) {
 				if (whiteturn) {
+					System.out.println(incomingString);
 					incomingString = streamIn.readUTF();
+					System.out.println(incomingString);
 					oldLoc = board.getTiles()[Integer.parseInt(incomingString.substring(0, 1))][Integer
 							.parseInt(incomingString.substring(1, 2))];
 					newLoc = board.getTiles()[Integer.parseInt(incomingString.substring(2, 3))][Integer
 							.parseInt(incomingString.substring(3, 4))];
-					board.getPiece(oldLoc).setLocation(newLoc);
+					if (oldLoc == null) {
+						System.out.println("could not find oldLoc");
+					} else {
+						p = board.getPiece(oldLoc);
+						if (p == null) {
+							System.out.println("could not find piece");
+						}
+					}
+					if (newLoc == null) {
+						System.out.println("could not find newLoc");
+
+					}
+					if (p != null && newLoc != null) {
+						board.movePieceTo(p, newLoc);
+					}
 				} else {
 					// get outgoingMove data from local board
 					outgoingString = "7172";
