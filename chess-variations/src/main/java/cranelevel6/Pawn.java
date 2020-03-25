3790c380f74ba4ce3c579b58fd4bcc31c984575d
TheWrencat <wrentolo@gmail.com>
2020-03-24 17:38:25 -0700

    .

--- chess-variations/src/main/java/cranelevel6/Pawn.java
+++ chess-variations/src/main/java/cranelevel6/Pawn.java
@@ -43,32 +43,6 @@ public class Pawn extends Piece {
 
 	}
 
-//this isn't working yet
-	class DoubleAdvanceRule extends AdvanceRule {
-		DoubleAdvanceRule(boolean white) {
-			super(white);
-		}
-
-		boolean isValid(Piece p, Location l) {
-			if (!super.isValid(p, l)) {
-				return false;
-			}
-			Piece lPiece = l.getPiece();
-			if (lPiece == null) {
-				Location nextLoc = lPiece.getTargetLocation(Directions.N);
-				if (nextLoc.getHasPiece() == false) {
-					return true;
-				} else {
-					return false;
-				}
-
-			} else {
-				return false;
-			}
-
-		}
-	}
-
 	Pawn(Board board) {
 		super(board);
 		type = "pawn";
@@ -78,14 +52,12 @@ public class Pawn extends Piece {
 		Rule blackTakeRule = new TakeRule(false);
 		Rule whiteAdvanceRule = new AdvanceRule(true);
 		Rule blackAdvanceRule = new AdvanceRule(false);
-		Rule whiteDoubleAdvanceRule = new DoubleAdvanceRule(true);
-		Rule blackDoubleAdvanceRule = new DoubleAdvanceRule(false);
 
 		moves.put(Directions.N, whiteAdvanceRule);
-		moves.put(Directions.NN, whiteDoubleAdvanceRule);
+		moves.put(Directions.NN, whiteAdvanceRule);
 		moves.put(Directions.NE, whiteTakeRule);
 		moves.put(Directions.NW, whiteTakeRule);
-		moves.put(Directions.SS, blackDoubleAdvanceRule);
+		moves.put(Directions.SS, blackAdvanceRule);
 		moves.put(Directions.S, blackAdvanceRule);
 		moves.put(Directions.SW, blackTakeRule);
 		moves.put(Directions.SE, blackTakeRule);
